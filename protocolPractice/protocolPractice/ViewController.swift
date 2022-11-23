b4c44d9dde58acfc8261710f494d99cdb5097937
Alex <set1>
2022-11-23 21:25:27 +0200

    My first commit

---
+++ protocolPractice/protocolPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  protocolPractice
+//
+//  Created by Alexandr Chernets on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
